081f27c3df864bf4f19f65eb884ad42154faa53f
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2022-09-05 22:16:21 +0200

    Reenable test

---
+++ src/test/java/org/iac2/service/fixing/service/IssueFixingServiceTest.java
@@ -0,0 +1,12 @@
+package org.iac2.service.fixing.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IssueFixingServiceTest {
+
+    @Test
+    void fixFirstIssue() {
+    }
+}
