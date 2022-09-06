2e044ee20b2de1bc10893ca747988b54d66776b2
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2022-09-06 08:59:45 +0200

    create unit test for fixing service

--- src/test/java/org/iac2/service/fixing/service/IssueFixingServiceTest.java
+++ src/test/java/org/iac2/service/fixing/service/IssueFixingServiceTest.java
@@ -1,12 +1,65 @@
 package org.iac2.service.fixing.service;
 
+import java.util.Collections;
+
+import org.iac2.common.model.InstanceModel;
+import org.iac2.entity.compliancejob.ComplianceJobEntity;
+import org.iac2.entity.compliancejob.execution.ExecutionEntity;
+import org.iac2.entity.compliancejob.issue.ComplianceIssueEntity;
+import org.iac2.entity.compliancejob.issue.IssueFixingReportEntity;
+import org.iac2.entity.productionsystem.ProductionSystemEntity;
+import org.iac2.service.fixing.plugin.manager.IssueFixingPluginManager;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class IssueFixingServiceTest {
+
+    @MockBean
+    IssueFixingPluginManager pluginManager;
+
+    @Autowired
+    IssueFixingService service;
 
     @Test
     void fixFirstIssue() {
+        Mockito.when(pluginManager.getPlugin(anyString()))
+                .thenReturn(new MockIssueFixingPlugin());
+
+        ProductionSystemEntity productionSystem = new ProductionSystemEntity(
+                "my super system",
+                "opentoscacontainer",
+                "opentosca-container-model-creation-plugin");
+        ComplianceJobEntity complianceJob = new ComplianceJobEntity(
+                "my super job",
+                "property-value-checker",
+                "mock",
+                productionSystem,
+                null,
+                null,
+                Collections.emptyList()
+        );
+        ExecutionEntity execution = new ExecutionEntity(complianceJob);
+
+        ComplianceIssueEntity issue = new ComplianceIssueEntity(
+                execution,
+                "something is wrong!",
+                "wrong-property-value");
+        InstanceModel instanceModel = new InstanceModel(null);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> service.fixFirstIssue(execution, instanceModel));
+        execution.getComplianceIssueEntities().add(issue);
+        IssueFixingReportEntity report = service.fixFirstIssue(execution, instanceModel);
+        Assertions.assertNotNull(report);
+        Assertions.assertTrue(report.getIsSuccessful());
+        Assertions.assertEquals("", report.getDescription());
     }
 }
---
+++ src/test/java/org/iac2/service/fixing/service/MockIssueFixingPlugin.java
@@ -0,0 +1,37 @@
+package org.iac2.service.fixing.service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.iac2.common.model.InstanceModel;
+import org.iac2.common.model.ProductionSystem;
+import org.iac2.common.model.compliancejob.issue.ComplianceIssue;
+import org.iac2.service.fixing.common.interfaces.IssueFixingPlugin;
+import org.iac2.service.fixing.common.model.IssueFixingReport;
+
+public class MockIssueFixingPlugin implements IssueFixingPlugin {
+    @Override
+    public String getIdentifier() {
+        return "mock";
+    }
+
+    @Override
+    public boolean isSuitableForIssue(ComplianceIssue issue) {
+        return true;
+    }
+
+    @Override
+    public boolean isSuitableForProductionSystem(ProductionSystem productionSystem) {
+        return true;
+    }
+
+    @Override
+    public Collection<String> getRequiredPropertyNames() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public IssueFixingReport fixIssue(ComplianceIssue issue, InstanceModel model, ProductionSystem productionSystem) {
+        return new IssueFixingReport(true);
+    }
+}
