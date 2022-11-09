461cc1f8ea64cc9db4d9e2937821781eb51a9025
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2022-11-09 23:17:27 +0100

    Add alternative edmm manipulation methods
    Add methods that directly work on the underlying entitygraph

--- src/main/java/org/iac2/service/utility/Edmm.java
+++ src/main/java/org/iac2/service/utility/Edmm.java
@@ -1,25 +1,115 @@
 package org.iac2.service.utility;
 
-import com.google.common.collect.Maps;
-import io.github.edmm.model.DeploymentModel;
-import io.github.edmm.model.Property;
-import io.github.edmm.model.component.*;
-import io.github.edmm.model.relation.ConnectsTo;
-import io.github.edmm.model.relation.HostedOn;
-import io.github.edmm.model.relation.RootRelation;
-import io.github.edmm.model.support.EdmmYamlBuilder;
-import org.apache.catalina.Host;
-import org.opentosca.container.client.model.ApplicationInstance;
-import org.opentosca.container.client.model.NodeInstance;
-import org.opentosca.container.client.model.RelationInstance;
-
-import javax.persistence.criteria.Root;
+import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.google.common.collect.Maps;
+import io.github.edmm.core.parser.Entity;
+import io.github.edmm.core.parser.EntityGraph;
+import io.github.edmm.core.parser.EntityId;
+import io.github.edmm.core.parser.MappingEntity;
+import io.github.edmm.core.parser.ScalarEntity;
+import io.github.edmm.core.parser.SequenceEntity;
+import io.github.edmm.core.parser.support.DefaultKeys;
+import io.github.edmm.model.DeploymentModel;
+import io.github.edmm.model.component.RootComponent;
+import io.github.edmm.model.relation.ConnectsTo;
+import io.github.edmm.model.relation.DependsOn;
+import io.github.edmm.model.relation.HostedOn;
+import io.github.edmm.model.relation.RootRelation;
+import io.github.edmm.model.support.Attribute;
+import io.github.edmm.model.support.EdmmYamlBuilder;
+import io.github.edmm.model.support.ModelEntity;
+import io.github.edmm.model.support.TypeResolver;
 
 public class Edmm {
+
+    public static void addType(DeploymentModel deploymentModel, Class<? extends ModelEntity> componentType) throws IllegalAccessException {
+        Set<Entity> existingTypes = deploymentModel.getGraph().getChildren(EntityGraph.COMPONENT_TYPES);
+
+        // we couldn't find the type in the deployment model
+        if (existingTypes.stream().noneMatch(e -> e.getName().equals(TypeResolver.resolve(componentType)))) {
+            EntityGraph graph = deploymentModel.getGraph();
+            final EntityId typeId = EntityGraph.COMPONENT_TYPES.extend(TypeResolver.resolve(componentType));
+            graph.addEntity(new MappingEntity(typeId, graph));
+
+            // only the attributes in child types of RootComponent and DependsOn are properties
+            // also: if we are at the root component/relation type level, we have null as a parent type
+            if (!componentType.equals(RootComponent.class) && !componentType.equals(DependsOn.class)) {
+                // we are sure this is going to work, because we handle the case in which the current componentType is the
+                // root type separately.
+                Class<? extends ModelEntity> parentType = (Class<? extends ModelEntity>) componentType.getSuperclass();
+                graph.addEntity(new ScalarEntity(TypeResolver.resolve(parentType), typeId.extend(DefaultKeys.EXTENDS), graph));
+                Collection<Field> typeAttributesAsFields = Stream
+                        .of(componentType.getDeclaredFields())
+                        .filter(f -> f.getType() == Attribute.class)
+                        .toList();
+
+                if (typeAttributesAsFields.size() > 0) {
+                    final EntityId propertiesId = typeId.extend(DefaultKeys.PROPERTIES);
+                    graph.addEntity(new MappingEntity(propertiesId, graph));
+                    Attribute<?> attribute;
+
+                    for (Field typeAttributesAsField : typeAttributesAsFields) {
+                        attribute = (Attribute<?>) typeAttributesAsField.get(null);
+                        addPropertyDefinition(graph, attribute, propertiesId);
+                    }
+                }
+
+                addType(deploymentModel, parentType);
+            } else {
+                graph.addEntity(new ScalarEntity("null", typeId.extend(DefaultKeys.EXTENDS), graph));
+            }
+        }
+    }
+
+    private static void addPropertyDefinition(EntityGraph graph, Attribute<?> attribute, EntityId propertiesId) {
+        EntityId propertyId = propertiesId.extend(attribute.getName());
+        String edmmType = switch (attribute.getType().getName()) {
+            case "java.lang.Integer", "java.lang.Long" -> DefaultKeys.INTEGER;
+            case "java.lang.Float", "java.lang.Double" -> DefaultKeys.FLOAT;
+            default -> DefaultKeys.STRING;
+        };
+
+        graph.addEntity(new ScalarEntity(edmmType, propertyId, graph));
+    }
+
+    public static void addRelation(DeploymentModel deploymentModel,
+                                   EntityId startComponentEntityId,
+                                   EntityId targetComponentEntityId,
+                                   Class<? extends RootRelation> relationType) throws IllegalAccessException {
+        addType(deploymentModel, relationType);
+        EntityId relationsEntityId = startComponentEntityId.extend(DefaultKeys.RELATIONS);
+        EntityGraph graph = deploymentModel.getGraph();
+        graph.addEntity(
+                new SequenceEntity(relationsEntityId, graph));
+        graph.addEntity(
+                new ScalarEntity(targetComponentEntityId.getName(), relationsEntityId.extend(TypeResolver.resolve(relationType)), graph));
+    }
+
+    public static EntityId addComponent(DeploymentModel deploymentModel,
+                                        String componentId,
+                                        Map<String, String> attributeAssignments,
+                                        Class<? extends RootComponent> componentType) throws IllegalAccessException {
+        addType(deploymentModel, componentType);
+        EntityGraph graph = deploymentModel.getGraph();
+        EntityId componentEntityId = EntityGraph.COMPONENTS.extend(componentId);
+        graph.addEntity(new MappingEntity(componentEntityId, graph));
+        graph.addEntity(new ScalarEntity(TypeResolver.resolve(componentType), componentEntityId.extend(DefaultKeys.TYPE), graph));
+
+        if (attributeAssignments.size() > 0) {
+            // ideally, we should check whether these attributes are present in the component type declaration as properties
+            EntityId propertiesId = componentEntityId.extend(DefaultKeys.PROPERTIES);
+            graph.addEntity(new MappingEntity(propertiesId, graph));
+            attributeAssignments.forEach((key, value) -> graph.addEntity(new ScalarEntity(value, propertiesId.extend(key), graph)));
+        }
+
+        return componentEntityId;
+    }
 
     public static DeploymentModel addComponent(DeploymentModel deploymentModel,
                                                RootComponent hostingComponent,
@@ -51,7 +141,6 @@ public class Edmm {
             yamlBuilder.connectsTo(hostingComponent.getClass(), hostingComponent.getId());
         }
 
-
         String yamlString = yamlBuilder.build();
 
         DeploymentModel newDeploymentModel = DeploymentModel.of(yamlString);
@@ -71,5 +160,4 @@ public class Edmm {
     public static Collection<RootComponent> getDockerEngineComponents(DeploymentModel deploymentModel) {
         return deploymentModel.getComponents().stream().filter(c -> c.getProperties().containsKey("DockerEngineURL")).collect(Collectors.toList());
     }
-
 }
---
+++ src/test/java/org/iac2/service/utility/EdmmTest.java
@@ -0,0 +1,83 @@
+package org.iac2.service.utility;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Optional;
+
+import io.github.edmm.core.parser.Entity;
+import io.github.edmm.core.parser.EntityGraph;
+import io.github.edmm.core.parser.EntityId;
+import io.github.edmm.core.parser.MappingEntity;
+import io.github.edmm.core.parser.ScalarEntity;
+import io.github.edmm.core.parser.support.DefaultKeys;
+import io.github.edmm.model.DeploymentModel;
+import io.github.edmm.model.component.AwsBeanstalk;
+import io.github.edmm.model.component.Compute;
+import io.github.edmm.model.component.Paas;
+import io.github.edmm.model.component.Platform;
+import io.github.edmm.model.component.RootComponent;
+import io.github.edmm.model.relation.ConnectsTo;
+import io.github.edmm.model.relation.DependsOn;
+import io.github.edmm.model.support.TypeResolver;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+class EdmmTest {
+
+    @Test
+    void testAddType() throws IOException, IllegalAccessException {
+        ClassPathResource resource = new ClassPathResource("edmm/four-components-hosted-on.yml");
+        DeploymentModel model = DeploymentModel.of(resource.getFile());
+        Edmm.addType(model, AwsBeanstalk.class);
+        Assertions.assertTrue(
+                model.getGraph().getEntity(EntityGraph.COMPONENT_TYPES.extend(TypeResolver.resolve(AwsBeanstalk.class))).isPresent());
+        Assertions.assertTrue(
+                model.getGraph().getEntity(EntityGraph.COMPONENT_TYPES.extend(TypeResolver.resolve(Paas.class))).isPresent());
+        Assertions.assertTrue(
+                model.getGraph().getEntity(EntityGraph.COMPONENT_TYPES.extend(TypeResolver.resolve(Platform.class))).isPresent());
+        Assertions.assertTrue(
+                model.getGraph().getEntity(EntityGraph.COMPONENT_TYPES.extend(TypeResolver.resolve(RootComponent.class))).isPresent());
+    }
+
+    @Test
+    void testAddRelation() throws IOException, IllegalAccessException {
+        ClassPathResource resource = new ClassPathResource("edmm/four-components-hosted-on.yml");
+        DeploymentModel model = DeploymentModel.of(resource.getFile());
+        EntityId source = EntityGraph.COMPONENTS.extend("windows");
+        EntityId target = EntityGraph.COMPONENTS.extend("ubuntu");
+        Edmm.addRelation(model, source, target, ConnectsTo.class);
+        Assertions.assertTrue(
+                model.getGraph().getEntity(EntityGraph.RELATION_TYPES.extend(TypeResolver.resolve(ConnectsTo.class))).isPresent());
+        Assertions.assertTrue(
+                model.getGraph().getEntity(EntityGraph.RELATION_TYPES.extend(TypeResolver.resolve(DependsOn.class))).isPresent());
+        Optional<Entity> entity = model.getGraph().getEntity(
+                EntityGraph
+                        .COMPONENTS
+                        .extend("windows")
+                        .extend("relations")
+                        .extend(TypeResolver.resolve(ConnectsTo.class)));
+        Assertions.assertTrue(entity.isPresent());
+        Assertions.assertTrue(entity.get() instanceof ScalarEntity);
+        Assertions.assertEquals("ubuntu", ((ScalarEntity) entity.get()).getValue());
+    }
+
+    @Test
+    void testAddComponent() throws IOException, IllegalAccessException {
+        ClassPathResource resource = new ClassPathResource("edmm/four-components-hosted-on.yml");
+        DeploymentModel model = DeploymentModel.of(resource.getFile());
+        EntityId id = Edmm.addComponent(model, "flip-flop-machine", Map.of("os_family", "flip-flop-os"), Compute.class);
+        Assertions.assertEquals("flip-flop-machine", id.getName());
+        Optional<Entity> properties = model.getGraph().getEntity(id.extend(DefaultKeys.PROPERTIES));
+        Assertions.assertTrue(properties.isPresent());
+        Assertions.assertTrue(properties.get() instanceof MappingEntity);
+        Optional<Entity> property = model.getGraph().getEntity(id.extend(DefaultKeys.PROPERTIES).extend("os_family"));
+        Assertions.assertTrue(property.isPresent());
+        Assertions.assertTrue(property.get() instanceof ScalarEntity);
+        Assertions.assertEquals("flip-flop-os", ((ScalarEntity) property.get()).getValue());
+        Optional<Entity> type = model.getGraph().getEntity(id.extend(DefaultKeys.TYPE));
+        Assertions.assertTrue(type.isPresent());
+        Assertions.assertTrue(type.get() instanceof ScalarEntity);
+        Assertions.assertEquals(TypeResolver.resolve(Compute.class), ((ScalarEntity) type.get()).getValue());
+    }
+}
