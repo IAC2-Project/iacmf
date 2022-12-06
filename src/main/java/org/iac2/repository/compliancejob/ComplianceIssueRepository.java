25d059d6aae3d2cfe3cec96cba373cd61b9d5794
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2022-12-06 17:30:31 +0100

    disable exporting certain repository methods as REST methods

--- src/main/java/org/iac2/repository/compliancejob/ComplianceIssueRepository.java
+++ src/main/java/org/iac2/repository/compliancejob/ComplianceIssueRepository.java
@@ -6,8 +6,37 @@ import org.iac2.entity.compliancejob.execution.ExecutionEntity;
 import org.iac2.entity.compliancejob.issue.ComplianceIssueEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "compliance-issues")
 public interface ComplianceIssueRepository extends CrudRepository<ComplianceIssueEntity, Long> {
     List<ComplianceIssueEntity> findByExecution(ExecutionEntity execution);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends ComplianceIssueEntity> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends ComplianceIssueEntity> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Long aLong);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(ComplianceIssueEntity entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends Long> longs);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends ComplianceIssueEntity> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
