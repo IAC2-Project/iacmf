25fa8cfae1d732d7010d2bd38a473e1e4df17fc5
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2022-12-20 15:43:24 +0100

    Update ManualModelCreatorPlugin.java

--- src/main/java/org/iac2/service/architecturereconstruction/plugin/implementation/manual/ManualModelCreatorPlugin.java
+++ src/main/java/org/iac2/service/architecturereconstruction/plugin/implementation/manual/ManualModelCreatorPlugin.java
@@ -2,6 +2,7 @@ package org.iac2.service.architecturereconstruction.plugin.implementation.manual
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import org.iac2.common.exception.IaCTechnologyNotSupportedException;
 import org.iac2.common.model.InstanceModel;
@@ -10,26 +11,35 @@ import org.iac2.service.architecturereconstruction.common.interfaces.ModelCreati
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/***
+ * Refers to an externally-created EDMM instance model.
+ */
 public class ManualModelCreatorPlugin implements ModelCreationPlugin {
     private static final Logger LOGGER = LoggerFactory.getLogger(ManualModelCreatorPlugin.class);
+    private static final String CONFIG_ENTRY_MODEL_PATH = "modelPath";
+
+    private String modelPath;
+
     @Override
     public String getIdentifier() {
-        return "manualplugin";
+        return "manual-model-creation-plugin";
     }
 
     @Override
     public boolean isIaCTechnologySupported(String iacTechnologyName) {
-        return iacTechnologyName.equalsIgnoreCase("opentoscacontainer");
+        return true;
     }
 
     @Override
     public Collection<String> getRequiredConfigurationEntryNames() {
-        return Collections.emptyList();
+        return List.of(CONFIG_ENTRY_MODEL_PATH);
     }
 
     @Override
     public void setConfigurationEntry(String inputName, String inputValue) {
-        LOGGER.warn("Trying to pass user input to a plugin that does not need user inputs!");
+        if (inputName.equals(CONFIG_ENTRY_MODEL_PATH)) {
+
+        }
     }
 
     @Override
