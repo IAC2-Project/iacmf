eeaec5e54642f2060c3b7168cbf6b99f040570d2
Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>
2023-01-17 11:25:51 +0100

    adds fix to CORS issues with the execution controller
    Signed-off-by: Kálmán Képes <kalman.kepes@iaas.uni-stuttgart.de>

--- src/main/java/org/iac2/api/controller/ExecutionController.java
+++ src/main/java/org/iac2/api/controller/ExecutionController.java
@@ -13,16 +13,12 @@ import org.iac2.repository.compliancejob.ComplianceJobRepository;
 import org.iac2.repository.compliancejob.ExecutionRepository;
 import org.iac2.service.execution.ExecutionService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("executions")
 @Tag(name = "execution")
+@CrossOrigin( origins = "*" , allowedHeaders = "*" , methods = {RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.OPTIONS})
 public class ExecutionController {
     private final ExecutionService service;
     private final ComplianceJobRepository complianceJobRepository;
